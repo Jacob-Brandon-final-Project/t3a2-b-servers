518763e3ab71dc32097d66619bb6a18680733524
Brandon Powell <14297@coderacademy.edu.au>
2024-01-03 19:07:42 +1000

    changed port back

--- src/server.js
+++ src/server.js
@@ -84,7 +84,7 @@ app.post("/login", (request, response) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
